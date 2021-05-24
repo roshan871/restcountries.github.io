0951bad449961edec94af62932241763b3e6ebe3
roshan Dahal <roshandahal747@gmail.com>
2021-05-23 20:02:14 -0600

    Internal js file is converted to external js file

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-fetch(`https://restcountries.eu/rest/v2/all`)
-.then((x)=>x.json)
-.then((data)=>{
-    var rs = "";
-    for ( i in data){
-        rs += "<tr>";
-        rs += "<td>" + data[i].name + "</td>";
-        rs += "</tr>";
-    }
-});
+$.ajax({
+    url:"https://restcountries.eu/rest/v2/all",
+    type: "get",
+    success: function(data){
+        var rs = "";
+        var c = 0;
+        for (i in data){
+            rs += "<tr>";
+                c +=1;
+            rs += "<td>" + c + "</td>";
+            rs += "<td>" + data[i].name + "</td>";
+            rs += "<td>" + data[i].capital + "</td>";
+            rs += "<td>" + data[i].population + "</td>";
+            rs += "<td>" + data[i].timezones + "</td>";
+
+            rs += "<td><ol>";
+            for(j in data[i].borders){
+                rs += "<li>" + data[i].borders[j]+"</li>";
+            }
+            rs += "</ol></td>";
+            rs += "<td><image src='"+data[i].flag+"' style='height:100px;width:80%'/></td>";
+            rs += "</tr>";
+         }
+         document.getElementById('result').innerHTML = rs;
+        }
+    })
